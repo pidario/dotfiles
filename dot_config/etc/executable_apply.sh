992c3a10b9cdd955275c25b6aa7d978b4b4d85a4
pidario <3647292+pidario@users.noreply.github.com>
2021-10-05 11:48:18 +0200

    removed sddm and added dialogrc

--- dot_config/etc/executable_apply.sh
+++ dot_config/etc/executable_apply.sh
@@ -10,12 +10,12 @@ echo "source directory: $SOURCE"
 
 FIREFOX_PATH=/etc/firefox/policies
 CHROMIUM_PATH=/etc/chromium/policies/managed
-SDDM_PATH=/etc/sddm.conf.d
+DIALOG_PATH=/etc/dialogrc
 
-mkdir -p "$FIREFOX_PATH" "$CHROMIUM_PATH" "$SDDM_PATH"
+mkdir -p "$FIREFOX_PATH" "$CHROMIUM_PATH"
 
 cp "$SOURCE$FIREFOX_PATH/"* "$FIREFOX_PATH/"
 cp "$SOURCE$CHROMIUM_PATH/"* "$CHROMIUM_PATH/"
-cp "$SOURCE$SDDM_PATH/"* "$SDDM_PATH/"
+cp "$SOURCE$DIALOG_PATH" "$DIALOG_PATH"
 
 echo "done"
