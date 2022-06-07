fb2e25c8980e29ff4cedeaf93bacf40994437e4b
pidario <3647292+pidario@users.noreply.github.com>
2022-06-07 12:44:12 +0200

    minor fix

--- dot_config/nvim/lua/treesitter.lua
+++ dot_config/nvim/lua/treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
 	--one of "all", "language", or a list of languages
 	ensure_installed = "all",
+	ignore_install = { "phpdoc" },
 	highlight = {
 		-- false will disable the whole extension
 		enable = true,
