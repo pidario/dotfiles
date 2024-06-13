58bc1ebcc00f735b90cb6ba678cc4d1dad7e3d74
pidario <3647292+pidario@users.noreply.github.com>
2024-06-13 15:38:01 +0200

    qb; ff removed deprecated policies and added canvas protection

--- dot_config/qutebrowser/config.py
+++ dot_config/qutebrowser/config.py
@@ -1,17 +1,17 @@
-startpage = 'https://searx.tiekoetter.com'
+startpage = "https://searx.tiekoetter.com"
 
 config.load_autoconfig(False)
 
 c.auto_save.session = True
 c.content.autoplay = False
-c.confirm_quit = ['always']
-c.content.cookies.accept = 'no-3rdparty'
-c.content.headers.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}'
-c.editor.command = ['st', '-e', 'vi', '{file}']
+c.confirm_quit = ["always"]
+c.content.cookies.accept = "no-3rdparty"
+c.content.headers.user_agent = (
+    "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/115.0"
+)
+c.editor.command = ["termite", "-e", "vi", "{file}"]
 c.url.default_page = startpage
-c.url.searchengines = {
-    'DEFAULT': f'{startpage}/search?q={{}}'
-}
+c.url.searchengines = {"DEFAULT": f"{startpage}/search?q={{}}"}
 
-config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
-config.set('content.cookies.accept', 'all', 'devtools://*')
+config.set("content.cookies.accept", "all", "chrome-devtools://*")
+config.set("content.cookies.accept", "all", "devtools://*")
