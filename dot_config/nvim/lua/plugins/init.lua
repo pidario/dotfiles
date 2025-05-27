a4a7f16df3ce7a201a1a179e8e9a72ddd36c6c12
pidario <3647292+pidario@users.noreply.github.com>
2025-05-27 11:57:06 +0200

    hardtime vim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,29 +1,29 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		-- event = 'BufWritePre', -- uncomment for format on save
+		opts = require "configs.conform",
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-    "rmagatti/auto-session",
-    lazy = false,
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require "configs.lspconfig"
+		end,
+	},
+	{
+		"rmagatti/auto-session",
+		lazy = false,
 
-    ---enables autocomplete for opts
-    ---@module "auto-session"
-    ---@type AutoSession.Config
-    opts = {
-      suppressed_dirs = { "~/", "/" },
-      -- log_level = 'debug',
-    },
-  },
+		---enables autocomplete for opts
+		---@module "auto-session"
+		---@type AutoSession.Config
+		opts = {
+			suppressed_dirs = { "~/", "/" },
+			-- log_level = 'debug',
+		},
+	},
 	{
 		"vim-test/vim-test",
 		cmd = { "TestNearest", "TestFile", "TestSuite", "TestLast", "TestVisit" },
@@ -37,5 +37,11 @@ return {
 			]]
 			vim.g["test#strategy"] = "toggleterm"
 		end,
+	},
+	{
+		"m4xshen/hardtime.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {},
 	},
 }
