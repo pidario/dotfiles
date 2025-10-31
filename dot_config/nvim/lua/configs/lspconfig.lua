faab6cf618432cf836fe961228a12d1913b4d72d
pidario <3647292+pidario@users.noreply.github.com>
2025-10-31 15:26:14 +0100

    lazyvim instead of nvchad

--- dot_config/nvim/lua/configs/lspconfig.lua
+++
@@ -1,9 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-vim.lsp.config("html", {})
-vim.lsp.config("cssls", {})
-vim.lsp.config("ts_ls", {})
-vim.lsp.config("angularls", {})
-
-local servers = { "html", "cssls", "ts_ls", "angularls" }
-vim.lsp.enable(servers)
